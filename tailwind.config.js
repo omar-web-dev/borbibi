70ef2c7005b7d9f0841ef39ee7dea979dfba48f1
omarwebdeve <omarf977865@gmail.com>
2023-03-07 06:51:53 +0600

    borbibi

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    daisyui: {
+      themes: [
+        {
+          mytheme: {
+            "primary": "#661AE6",
+            "secondary": "#D926AA",
+            "accent": "#1FB2A5",
+            "neutral": "#191D24",
+            "base-100": "#2A303C",
+            "info": "#3ABFF8",
+            "success": "#36D399",
+            "warning": "#FBBD23",
+            "error": "#F87272",
+          },
+        },
+      ],
+    },
+  },
+  plugins: [require("daisyui")],
+}
